cb56b4b979902b2af263b8d6d4a7c14bb776ae27
ferrywilana <ferrywilana1@gmail.com>
2023-01-03 22:36:56 +0700

    menambahkan file javascript

---
+++ Javascript DOM/DOM Selection/script.js
@@ -0,0 +1,13 @@
+// dom selection 
+//document.getElementById() -> mengembalikan satu nilai
+
+const judul = document.getElementById('judul');
+judul.style.color ='blue';
+judul.style.backgroundColor = 'gray';
+
+
+// document.getElementByTagName() 
+// -> HTML Collection 
+// mengembalikan nilai array
+
+const p = document.getElementsByTagName('p');
