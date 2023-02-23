b4ea73baea6ec8067bb2fb40b8e4aaa3bb824448
ferrywilana <ferrywilana1@gmail.com>
2023-02-23 19:03:32 +0700

    menambahkan hapus penumpang

--- Javascript/Control Flow/jurangan angkot/angkotkompleks.js
+++ Javascript/Control Flow/jurangan angkot/angkotkompleks.js
@@ -1,32 +1,33 @@
 // jumlah orang
-let penumpang = ['ferry',undefined,'human'];
+let penumpang = [];
 let tambahPenumpang = function(namaPenumpang,penumpang ){ 
-// jika angkot kosong
+    // jika angkot kosong
     if(penumpang.length == 0 ){
     // tambah penumpang di awal array
     penumpang.push(namaPenumpang);
     // kembalikasan isi array & keluar dari function
-    return penumpang ;
-    
+    return penumpang ;    
 }else {
+    
     // telusuri seluruh kursi dari awal
-        for( let i = 0 ; i < penumpang.length; i++){
-        // jika ada kursi kosong 
+        for( let i = 0 ; i < penumpang.length; i++){       
+         // jika ada kursi kosong 
         if(penumpang[i] == undefined ){
             // tambah penumpang di kursi tersebut
             penumpang[i] = namaPenumpang ;
+            namaPenumpang != namaPenumpang 
             // kembalikan isi array & keluar dari function
             return penumpang;
-            // jika sudah ada nama yang sama
+
+        // jika sudah ada nama yang sama
         }else if(penumpang[i] == namaPenumpang ){
             //tampilan pesan kesalahannya
             console.log(namaPenumpang + ' sudah ada di dalam angkot');
             // kembalikan isi array & keluar dari function
             return penumpang;
 
-        }
-        // jika seluruh kursi terisi
-        else if(i == penumpang.length - 1 ){ 
+         // jika seluruh kursi terisi
+        }else if(i == penumpang.length - 1 ){ 
             // tambah penumpang di akhir array
             penumpang.push(namaPenumpang);
             // kembalikan isi array & keluar dari function
@@ -35,3 +36,26 @@ let tambahPenumpang = function(namaPenumpang,penumpang ){
         }
     }
 }
+let hapusPenumpang = function(namaPenumpang,penumpang){
+// Penumpang kosong
+    if( penumpang.length == 0 ){
+        console.log('Angkot masih kosong');
+    }else{ 
+        for(let i = 0 ; i < penumpang.length ; i++){
+            if(penumpang[i] == namaPenumpang ){
+                penumpang[i] = undefined;
+            }else if( i == penumpang.length - i ){
+                console.log(namaPenumpang + 'tidak ada di dalam angkot');
+                return penumpang;
+            } 
+        }
+    }
+    return penumpang;
+}
+
+
+
+
+
+
+
