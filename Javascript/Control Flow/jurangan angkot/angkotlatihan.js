53f844beebdfcd690f80a8ff64d7b150309bf6e1
ferrywilana <ferrywilana1@gmail.com>
2023-03-01 07:14:13 +0700

    menammbahkan file test testan

--- Javascript/Control Flow/jurangan angkot/angkotlatihan.js
+++ Javascript/Control Flow/jurangan angkot/angkotlatihan.js
@@ -1,16 +1,41 @@
-let noAngkot = 1 ;
-let jumlahAngkot = 10 ;
-let angkotBeroperasi = 6 ;
 
-for ( noAngkot = 1 ; noAngkot <= jumlahAngkot ; noAngkot++ ) {
-    if(noAngkot <= angkotBeroperasi && noAngkot !== 1 ) {
-        console.log('No Angkot ' + noAngkot + ' beroperasi dengan baik');
-    }else if (noAngkot === 7 || noAngkot === 10 || noAngkot === 1){
-        console.log('No Angkot ' + noAngkot + ' Sedang lembur')
+let penumpang = [];
+
+let tambahPenumpang = function(namaPenumpang,penumpang){
+    if (penumpang == 0 ){
+        penumpang.push(namaPenumpang);
+        return penumpang;
+    }else{
+       for( let i = 0 ; i < penumpang.length ; i++){
+            // cek apakah kursi tersebut kosong atau tidak
+            // jika kosong 
+            if(penumpang[i] == undefined){
+                penumpang[i] = namaPenumpang ;
+                penumpang[i] != namaPenumpang ;
+                return penumpang ;
+            // jika penumpang orang dengan nama yang sama
+            }else if(penumpang[i] == namaPenumpang ){
+                console.log('penumpang sudah ada ');
+                return penumpang ;
+            // jika penumpang masih kosong dorong ke kursi sebelahnya    
+            }else if ( i == penumpang.length - 1) {
+                penumpang.push(namaPenumpang);
+                return penumpang;
+            }
+       }
     }
-    
-    
-    else {
-        console.log('No Angkot ' + noAngkot + ' Tidak beroperasi')
+}
+let hapusPenumpang = function(namaPenumpang,penumpang){
+    if (penumpang == 0 ){
+        console.log('belom ada penumpang ');
+    }else {
+        for(let i = 0 ; i < penumpang.length ; i++){
+            if(penumpang[i] == namaPenumpang ){
+                penumpang[i] == undefined 
+            }else if(penumpang[i] == penumpang.length - 1){
+                console.log('penumpang tidak ada');
+            }
+        }
     }
+    return penumpang
 }
